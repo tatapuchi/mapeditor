521d16a29589ff78bf439c4725e421787660b176
Vibhor Singh <vibdaddy12@gmail.com>
2020-03-01 14:29:32 +0100

    first commit

---
+++ desktop/src/com/mygdx/mapeditor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.mapeditor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.mapeditor.mapeditor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new mapeditor(), config);
+	}
+}
